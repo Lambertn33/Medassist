773fc8cd029745ebbb7a74111323e5079a600e2b
Lambert Nsengimana <lambertnsengimana@Lamberts-MacBook-Pro.local>
2025-11-13 20:21:01 +0200

    chore: add pointer behaviour to tabls

--- client/src/components/encounters/EncounterDetails.tsx
+++ client/src/components/encounters/EncounterDetails.tsx
@@ -254,7 +254,7 @@ export const EncounterDetails = ({ encounter }: { encounter: IEncounter }) => {
                 ${
                   activeTab === tab.id
                     ? 'border-blue-600 text-blue-600'
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                    : 'border-transparent cursor-pointer text-gray-500 hover:text-gray-700 hover:border-gray-300'
                 }
               `}
             >
