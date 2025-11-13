84c082544b4f780d8819794f07f69df46a3c3502
Lambert Nsengimana <lambertnsengimana@Lamberts-MacBook-Pro.local>
2025-11-13 14:03:17 +0200

    feat: add direct link from dashboard to encounters

--- client/src/pages/dashboard/DashboardIndex.tsx
+++ client/src/pages/dashboard/DashboardIndex.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getDashboardData } from '@/api/dashboard';
 import { Loader, TotalsOverview, LatestEncounters } from '@/components';
 import type { IDashboardData } from '@/interfaces/dashboard/IDashboardData';
+import { Link } from 'react-router';
 
 export const DashboardIndex = () => {
     const { data, isLoading, error } = useQuery<IDashboardData>({
@@ -21,7 +22,10 @@ export const DashboardIndex = () => {
              : <div className="flex flex-col gap-6">
                 <TotalsOverview data={data} />
                 <div className="flex flex-col gap-6">
-                    <h2 className="text-3xl font-bold text-blue-600 mb-4">Latest Encounters</h2>
+                    <div className="flex justify-between items-center">
+                        <h2 className="text-3xl font-bold text-blue-600 mb-4">Latest Encounters</h2>
+                        <Link to="/dashboard/encounters" className="text-blue-600 font-bold hover:text-blue-800 transition-colors">View All Encounters</Link>
+                    </div>
                     <LatestEncounters data={data?.todayEncounters || []} />
                 </div>
             </div>}
